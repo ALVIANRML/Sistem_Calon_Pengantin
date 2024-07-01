ffc7433deedbdb053cd6f3890e6de5dd64dfc598
alvian <alvianrmli@gmail.com>
2024-07-01 12:51:18 +0700

    register & konfigurasi lainnya

---
+++ application/views/templates/Auth_footer.php
@@ -0,0 +1,24 @@
+ <!-- Bootstrap core JavaScript-->
+ <script src="<?= base_url('assets/') ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('assets/') ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?= base_url('assets/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url('assets/') ?>js/sb-admin-2.min.js"></script>
+
+	<!-- JS  -->
+	  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        // Function to show error modal
+        function showErrorModal() {
+            $('#errorModal').modal('show');
+        }
+    </script>
+
+</body>
+
+</html>
